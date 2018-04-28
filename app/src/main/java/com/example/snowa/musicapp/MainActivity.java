a4dc6a4b0a78792c705a85a1717711be554ad792
hyouka27 <nsawek@gmail.com>
2018-04-28 14:54:13 +0200

    Menu glowne

--- app/src/main/java/com/example/snowa/musicapp/MainActivity.java
+++ app/src/main/java/com/example/snowa/musicapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.snowa.musicapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button bTopSongs = findViewById(R.id.bTopSongs);
+        bTopSongs.setOnClickListener(new View.OnContextClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,
+                        "TODO", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
