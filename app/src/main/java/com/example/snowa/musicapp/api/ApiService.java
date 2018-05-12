a43f77f9d5400301b930f8c479633225d2db0884
hyouka27 <nsawek@gmail.com>
2018-05-12 15:16:05 +0200

    Gson

--- app/src/main/java/com/example/snowa/musicapp/api/ApiService.java
+++ app/src/main/java/com/example/snowa/musicapp/api/ApiService.java
@@ -7,6 +7,7 @@ public class ApiService {
     public static ApiClient getService() {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://www.theaudiodb.com/api/v1/json/1/")
+                .addConverterFactory(GsonConverterFactory.​create()) 
                 .build();
 
         return retrofit.create(ApiClient.class);
