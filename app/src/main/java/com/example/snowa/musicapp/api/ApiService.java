d9cd34fb80d5597a42ae7392d07db3875f460f53
hyouka27 <nsawek@gmail.com>
2018-05-12 20:56:26 +0200

    Gson poprawka.

--- app/src/main/java/com/example/snowa/musicapp/api/ApiService.java
+++ app/src/main/java/com/example/snowa/musicapp/api/ApiService.java
@@ -1,13 +1,14 @@
 package com.example.snowa.musicapp.api;
 
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiService {
 
     public static ApiClient getService() {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://www.theaudiodb.com/api/v1/json/1/")
-                .addConverterFactory(GsonConverterFactory.​create()) 
+                .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
         return retrofit.create(ApiClient.class);
