6e5ac3c54354fd7051201b61ee7e11e4a0200423
hyouka27 <nsawek@gmail.com>
2018-05-12 15:11:20 +0200

    Modele

---
+++ app/src/main/java/com/example/snowa/musicapp/api/TrendingList.java
@@ -0,0 +1,8 @@
+package com.example.snowa.musicapp.api;
+
+import java.util.List;
+
+public class TrendingList {
+    ​
+    public List<TrendingSingle> trending​;
+}
---
+++ app/src/main/java/com/example/snowa/musicapp/api/TrendingSingle.java
@@ -0,0 +1,14 @@
+package com.example.snowa.musicapp.api;
+
+public class TrendingSingle {
+
+    public int idAlbum​;
+    public int idArtist​;
+    public int idTrack​;
+    public int idTrend​;
+    public int intChartPlace​;
+    public String strAlbum​;
+    public String strArtist​;
+    public String strTrack​;
+    public String strTrackThumb​;
+}
